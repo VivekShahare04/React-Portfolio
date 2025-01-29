7e10e6d2cc6ee9e9d05c46defded8134d70e843c
VivekShahare04 <mastervivek23@gmail.com>
2025-01-30 01:14:03 +0530

    update

---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"First Personal Portfolio",
+        description:"It is build with help of basic frameworks of web Development such as HTML,CSS & JavaScript,Tailwind,etc. that reprsents who am I?",
+        tags:["react","HTML&CSS","styledComponents","Javascript"],
+        
+        demo:"https://kukro.netlify.app/",
+        github:"https://github.com/VivekShahare04/Personal-Portfolio"
+    },
+    {
+        id:2,
+        name:"Lot parking Management",
+        description:"A parking Management website build in PHP an old language but very effective and still Greatest of all Time. With searching and filtering functionalities.",
+        tags:["Php","HTML&CSS","Javascript","Databases"],
+        
+        // demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        github:"https://github.com/VivekShahare04/Parking-lot-Management-System"
+        
+    },
+    {
+        id:3,
+        name:"Calorie Counter",
+        description:"It is used to detect calorie according to BMI(Body Mass Index)",
+        tags:["Machine Learning"],
+        
+        // demo:"http://devto-clone-codebucks27.vercel.app/",
+        github:"https://github.com/VivekShahare04/Calorie_counter"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+        github:"https://github.com/codebucks27/react-sidebar"
+    },
+    {
+        id:5,
+        name:"Pomodoro Timer(Chrome Extension)",
+        description:"An timer to track your activites",
+        tags:["Javascript"],
+        
+        // demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+        github:"https://github.com/VivekShahare04/Pomodoro-Timer"
+        
+    },
+    {
+        id:6,
+        name:"Emotion-based Music Recommendation System(Ongoing)",
+        description:"This system is used to classify the emotion of person and on the basis of this emotion it will suggest the music",
+        tags:["Machine Learning","OpenCV"],
+        
+        // demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+        github:"https://github.com/VivekShahare04/Emotion-based-Music-Recommendation-System"
+        
+    },
+] 
